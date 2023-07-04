93d91be3de495a3ed2175d899b4a38efe8a8727e
ksabry97 <isabryex@gmail.com>
2023-07-04 13:15:49 +0300

    'sumoftwonumbers'

---
+++ twoSums.js
@@ -0,0 +1,27 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+// solution :
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let arr = [];
+  let a;
+
+  for (let i = 0; i < nums.length; i++) {
+    a = target - nums[i];
+
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] == a) {
+        arr.push(i, j);
+      }
+    }
+  }
+  return arr;
+};
