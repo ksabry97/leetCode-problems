955cb3ed08bc77a046c834e475ee256d9d2eafc7
ksabry97 <isabryex@gmail.com>
2023-07-06 16:14:54 +0300

    'asdw'

---
+++ twoSum-11.js
@@ -0,0 +1,35 @@
+/**
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 < numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+ */
+
+//solution :
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let arr = [];
+  let n = numbers.length - 1;
+  let i = 0;
+
+  while (i < n) {
+    let sum = numbers[i] + numbers[n];
+    if (sum == target) {
+      arr.push(i + 1, n + 1);
+    }
+    if (sum > target) {
+      n--;
+    } else {
+      i++;
+    }
+  }
+  return arr;
+};
